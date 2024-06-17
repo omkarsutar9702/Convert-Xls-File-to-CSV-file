906da7bc6a720b863729b024fbe63187c771dbbf
omkarSutar9702 <omkarSutar115@gmail.com>
2024-06-17 20:14:42 +0530

    data added to sharepoint

--- XLS to XLSX.py
+++ XLS to XLSX.py
@@ -2,11 +2,12 @@
 # Import necessary modules
 from win32com import client
 import pandas as pd
+from sharepoint_utils import get_folder_urls
 from sharepoint_utils import connect_to_sharepoint
 from sharepoint_utils import upload_dataframe_to_sharepoint
 #%%
 # Define the file path
-file_path = r"C:\Users\HP\somaiya.edu\PyDataNinja - Python\C) Production\XLS to XLSX\Invoice Status Report.xls"
+file_path = r"C:\Users\HP\somaiya.edu\PyDataNinja - Python\C) Production\XLS to XLSX\Input xls file\Invoice Status Report.xls"
 #%%
 # Function to read Excel file and convert to DataFrame
 def read_excel_to_df(file_path):
@@ -38,4 +39,10 @@ df = read_excel_to_df(file_path)
 
 # %%
 df
+# %%
+ctx = connect_to_sharepoint('omkar.sutar@somaiya.edu' , 'Sunfl0wer@1234' , 'https://somaiya0.sharepoint.com/sites/PyDataNinja')
+# %%
+Upload_URL = '/sites/PyDataNinja/Python/C) Production/XLS to XLSX/ouput csv file'
+# %%
+upload_dataframe_to_sharepoint(ctx , Upload_URL , df , "Invoice summary report.csv")
 # %%
